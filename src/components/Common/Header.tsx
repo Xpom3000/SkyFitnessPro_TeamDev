baeaffc360a9bfb54eb6238a94a8236c1eb67f59
Igor Shipitko <garikrhip20@gmail.com>
2024-07-03 21:47:06 +0300

    dir1 начало начал

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
 import "../../css/style.css";
+import { appRoutes } from "../../lib/appRoutes";
 
 export default function Header() {
   return (
     <div className="container flex justify-between mx-auto">
       <div className="flex flex-col space-y-[15px]">
         <div>
-          {/* <Link to="/">
-            <img src="" alt="logo">
-          </Link> */}
+        <Link to={appRoutes.MAIN}>
+        <img src="../images/logo.png" alt="logo"/>
+      </Link>
         </div>
         <div>
           <p className="text-black">Онлайн тренировки для занятий дома</p>
--- src/components/DataComponents/FittingTexts.tsx
+++ src/components/DataComponents/FittingTexts.tsx
@@ -1,3 +1,45 @@
+import { Link } from "react-router-dom";
+// import MainPage from "../../pages/MainPage";
+import { appRoutes } from "../../lib/appRoutes";
+
 export default function FittingText() {
-  return;
+  return (
+    
+    <div className="container flex justify-between mx-auto">
+      <Link to={appRoutes.COURSE}>
+        <img src="images/logo.png" alt="logo"/>
+      </Link>
+        <h1>Подойдет для вас, если:</h1>
+      
+      <div className="container flex justify-between mx-auto">
+        <div>
+          <div>
+            <h2 className="rounded-buttonRadius mainColor text-mainColor  px-btnX py-btnY">
+              1
+            </h2>
+            <h2>Давно хотели попробовать йогу, но не решались начать</h2>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2 className="rounded-buttonRadius text-mainColor px-btnX py-btnY">
+              2
+            </h2>
+            <h2>
+              Хотите укрепить позвоночник, избавиться от болей в спине и
+              суставах
+            </h2>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2 className="rounded-buttonRadius text-mainColor px-btnX py-btnY">
+              3
+            </h2>
+            <h2>Ищете активность, полезную для тела и души</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
