8b8303d326892a7c4bde92417f9458959e86cb81
Igor Shipitko <garikrhip20@gmail.com>
2024-07-04 00:05:35 +0300

    dir1 готов

--- src/components/DataComponents/Directions.tsx
+++ src/components/DataComponents/Directions.tsx
@@ -1,10 +1,48 @@
+import { Link } from "react-router-dom";
+// import MainPage from "../../pages/MainPage";
+import { appRoutes } from "../../lib/appRoutes";
 export default function Directions() {
     return (
-      <div>
-        <div>
-          <h1>Направления</h1>
-          <h2>Давно хотели попробовать йогу, но не решались начать</h2>
+      <div className="container flex flex-col mx-auto">
+         <Link to={appRoutes.COURSE}>
+        <img src="images/logo.png" alt="logo" />
+      </Link>
+      <p className="pb-10 font-bold text-5xl p-5">
+        Подойдет для вас, если:
+      </p>
+      <div className="container flex align-items-center mx-auto">
+        <div className=" p-[10px] w-4/12">
+          <div className="h-36 flex items-centr bg-black rounded-blockRadiusFrame">
+            <div className="flex items-center p-3">
+              <h2 className=" text-mainColor font-bold text-6xl pr-5">1</h2>
+              <h2 className=" text-white">
+                Давно хотели попробовать йогу, но не решались начать
+              </h2>
+            </div>
+          </div>
         </div>
+        <div className=" p-[10px] w-4/12">
+          <div className="h-36 flex items-center  bg-black rounded-blockRadiusFrame">
+            <div className="flex items-center p-3">
+              <h2 className=" text-mainColor font-bold text-6xl pr-5">2</h2>
+              <h2 className=" text-white">
+                Хотите укрепить позвоночник, избавиться от болей в спине и
+                суставах
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className=" p-[10px] w-4/12">
+          <div className="h-36 flex items-center  bg-black rounded-blockRadiusFrame">
+            <div className="flex items-center p-3">
+              <h2 className=" text-mainColor font-bold text-6xl pr-5">3</h2>
+              <h2 className=" text-white">
+                Ищете активность, полезную для тела и души
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     );
   }
--- src/components/DataComponents/FittingTexts.tsx
+++ src/components/DataComponents/FittingTexts.tsx
@@ -1,42 +1,41 @@
-import { Link } from "react-router-dom";
 // import MainPage from "../../pages/MainPage";
-import { appRoutes } from "../../lib/appRoutes";
 
 export default function FittingText() {
   return (
-    
-    <div className="container flex justify-between mx-auto">
-      <Link to={appRoutes.COURSE}>
-        <img src="images/logo.png" alt="logo"/>
-      </Link>
-        <h1>Подойдет для вас, если:</h1>
-      
-      <div className="container flex justify-between mx-auto">
-        <div>
-          <div>
-            <h2 className="rounded-buttonRadius mainColor text-mainColor  px-btnX py-btnY">
-              1
-            </h2>
-            <h2>Давно хотели попробовать йогу, но не решались начать</h2>
+    <div className="container flex flex-col mx-auto">
+      <p className="pb-10 font-bold text-5xl p-5">
+        Подойдет для вас, если:
+      </p>
+      <div className="container flex align-items-center mx-auto">
+        <div className=" p-[10px] w-4/12">
+          <div className="h-36 flex items-centr bg-black rounded-blockRadiusFrame">
+            <div className="flex items-center p-3">
+              <h2 className=" text-mainColor font-bold text-6xl pr-5">1</h2>
+              <h2 className=" text-white">
+                Давно хотели попробовать йогу, но не решались начать
+              </h2>
+            </div>
           </div>
         </div>
-        <div>
-          <div>
-            <h2 className="rounded-buttonRadius text-mainColor px-btnX py-btnY">
-              2
-            </h2>
-            <h2>
-              Хотите укрепить позвоночник, избавиться от болей в спине и
-              суставах
-            </h2>
+        <div className=" p-[10px] w-4/12">
+          <div className="h-36 flex items-center  bg-black rounded-blockRadiusFrame">
+            <div className="flex items-center p-3">
+              <h2 className=" text-mainColor font-bold text-6xl pr-5">2</h2>
+              <h2 className=" text-white">
+                Хотите укрепить позвоночник, избавиться от болей в спине и
+                суставах
+              </h2>
+            </div>
           </div>
         </div>
-        <div>
-          <div>
-            <h2 className="rounded-buttonRadius text-mainColor px-btnX py-btnY">
-              3
-            </h2>
-            <h2>Ищете активность, полезную для тела и души</h2>
+        <div className=" p-[10px] w-4/12">
+          <div className="h-36 flex items-center  bg-black rounded-blockRadiusFrame">
+            <div className="flex items-center p-3">
+              <h2 className=" text-mainColor font-bold text-6xl pr-5">3</h2>
+              <h2 className=" text-white">
+                Ищете активность, полезную для тела и души
+              </h2>
+            </div>
           </div>
         </div>
       </div>
